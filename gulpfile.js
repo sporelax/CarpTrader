0b1f3bc05126a69d81c11a5b61517060c479e211
sporelax <perik911@gmail.com>
2017-05-06 23:01:16 +0200

    fixed gulpfile, started react trial

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,39 @@
-var gulp = require('gulp'),
-   uglify = require('gulp-uglify');
+const gulp        = require('gulp');
+const postcss     = require('gulp-postcss');
+const sourcemaps  = require('gulp-sourcemaps');
+const autoprefixer= require('autoprefixer');
+const uglify      = require('gulp-uglify');
+const babel       = require('gulp-babel');
+const del         = require('del');
 
-gulp.task('minify', function () {
-   gulp.src('src/app.js')
-      .pipe(uglify())
+gulp.task('clean', function() {
+  return del(['build']);
+});
+
+gulp.task('scripts', function () {
+      gulp.src('src/*.js')
+      .pipe(babel( {
+            presets: ['es2015','react']
+      }))
+      //.pipe(uglify())
       .pipe(gulp.dest('build'))
 });
+
+gulp.task('htmls', function() {
+      gulp.src('src/*.html')
+      .pipe(gulp.dest('build'))
+})
+
+gulp.task('autoprefixer', function () {
+    return gulp.src('./src/*.css')
+        .pipe(postcss([ autoprefixer() ]))
+        .pipe(gulp.dest('./build'));
+});
+
+gulp.task('watch', function() {
+      gulp.watch('src/*.js',['scripts']);
+      gulp.watch('src/*.css',['autoprefixer']);
+      gulp.watch('src/*.html',['htmls']);
+})
+
+gulp.task('default', ['watch', 'clean', 'htmls', 'scripts', 'autoprefixer']);
--- src/main.js
+++ src/main.js
@@ -7,11 +7,7 @@ function createWindow () {
   win = new BrowserWindow({width: 800, height: 600})
 
   // and load the index.html of the app.
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, '/dist/index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+  win.loadURL('file://' + __dirname + '/index.html');
 
   // Open the DevTools.
   win.webContents.openDevTools()
