f512c5e6e6726a00c73c870d69ed20cdc3032588
sporelax <perik911@gmail.com>
2017-05-07 08:18:31 +0200

    react tutorial working with livereload etc. Starting point

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
 const gulp        = require('gulp');
 const postcss     = require('gulp-postcss');
 const sourcemaps  = require('gulp-sourcemaps');
+const livereload  = require('gulp-livereload');
 const autoprefixer= require('autoprefixer');
 const uglify      = require('gulp-uglify');
 const babel       = require('gulp-babel');
 const del         = require('del');
 
 gulp.task('clean', function() {
-  return del(['build']);
-});
+      return del(['build'])
+})
 
 gulp.task('scripts', function () {
       gulp.src('src/*.js')
@@ -17,20 +18,23 @@ gulp.task('scripts', function () {
       }))
       //.pipe(uglify())
       .pipe(gulp.dest('build'))
-});
+})
 
 gulp.task('htmls', function() {
       gulp.src('src/*.html')
       .pipe(gulp.dest('build'))
+      .pipe(livereload())
 })
 
 gulp.task('autoprefixer', function () {
     return gulp.src('./src/*.css')
         .pipe(postcss([ autoprefixer() ]))
-        .pipe(gulp.dest('./build'));
-});
+        .pipe(gulp.dest('./build'))
+        .pipe(livereload())
+})
 
 gulp.task('watch', function() {
+      livereload.listen();
       gulp.watch('src/*.js',['scripts']);
       gulp.watch('src/*.css',['autoprefixer']);
       gulp.watch('src/*.html',['htmls']);
